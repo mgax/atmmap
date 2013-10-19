609d548d5e9f29b1f5c55f2670a5cf0541aa6556
Alex Morega <alex@grep.ro>
2013-10-19 23:17:20 +0300

    download banks

--- manage.py
+++ manage.py
@@ -2,6 +2,13 @@
 
 import flask
 from flask.ext.script import Manager
+import requests
+
+BBOX = {'E': 26.11, 'N': 44.45, 'W': 26.07, 'S': 44.42}
+DATA_URL = 'http://overpass.osm.rambler.ru/cgi/interpreter'
+BANK_QUERY = ("[out:json];{filter}"
+              "({BBOX[S]},{BBOX[W]},{BBOX[N]},{BBOX[E]});"
+              "out;")
 
 views = flask.Blueprint('views', __name__)
 
@@ -19,6 +26,27 @@ def create_app():
 
 def create_manager(app):
     manager = Manager(app)
+
+    @manager.command
+    def download():
+        query = BANK_QUERY.format(
+            BBOX=BBOX,
+            filter="node[amenity=bank][atm=yes]",
+        )
+        resp = requests.get(DATA_URL, params={'data': query})
+
+        data = []
+        for elem in resp.json()['elements']:
+            data.append({
+                'lat': elem['lat'],
+                'lon': elem['lon'],
+                'name': elem['tags'].get('name'),
+            })
+
+        data_path = app.static_folder + '/data.json'
+        with open(data_path, 'w', encoding='utf-8') as f:
+            flask.json.dump(data, f, indent=2, sort_keys=True)
+
     return manager
 
 
